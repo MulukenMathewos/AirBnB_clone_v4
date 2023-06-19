613c0c87d6d9c36b6a478b5b57d2c5ed8e6f3107
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-06-20 00:43:01 +0300

    Update test_with_MySQL.py

--- test_with_MySQL.py
+++ test_with_MySQL.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import pymysql
 import os
 
 # Get the values from environment variables
@@ -9,24 +8,19 @@ user = os.environ.get('HBNB_MYSQL_USER')
 password = os.environ.get('HBNB_MYSQL_PWD')
 database = os.environ.get('HBNB_MYSQL_DB')
 
-# Connect to the MySQL database
-connection = pymysql.connect(host=host, user=user, password=password, database=database)
-cursor = connection.cursor()
-
 # Get the initial count of records in the table
-cursor.execute("SELECT COUNT(*) FROM states")
-initial_count = cursor.fetchone()[0]
+initial_count_command = f"mysql -h {host} -u {user} -p{password} -D {database} -e 'SELECT COUNT(*) FROM states;'"
+initial_count_output = os.popen(initial_count_command).read().strip()
+initial_count = int(initial_count_output.split('\n')[1])
 
 # Execute the command to create a new state
-cursor.execute("INSERT INTO states (name) VALUES ('California')")
+create_state_command = f"mysql -h {host} -u {user} -p{password} -D {database} -e 'INSERT INTO states (name) VALUES (\"California\");'"
+os.system(create_state_command)
 
 # Get the updated count of records in the table
-cursor.execute("SELECT COUNT(*) FROM states")
-updated_count = cursor.fetchone()[0]
+updated_count_command = f"mysql -h {host} -u {user} -p{password} -D {database} -e 'SELECT COUNT(*) FROM states;'"
+updated_count_output = os.popen(updated_count_command).read().strip()
+updated_count = int(updated_count_output.split('\n')[1])
 
 # Assert the difference is equal to 1
 assert updated_count - initial_count == 1
-
-# Close the database connection
-cursor.close()
-connection.close()
