c46a113355db8309f35142da87316e3342eeefd9
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-06-20 00:47:09 +0300

    Update test_with_MySQL.py

--- test_with_MySQL.py
+++ test_with_MySQL.py
@@ -9,16 +9,16 @@ password = os.environ.get('HBNB_MYSQL_PWD')
 database = os.environ.get('HBNB_MYSQL_DB')
 
 # Get the initial count of records in the table
-initial_count_command = f"mysql -h {host} -u {user} -p{password} -D {database} -e 'SELECT COUNT(*) FROM states;'"
+initial_count_command = "mysql -h {} -u {} -p{} -D {} -e 'SELECT COUNT(*) FROM states;'".format(host, user, password, database)
 initial_count_output = os.popen(initial_count_command).read().strip()
 initial_count = int(initial_count_output.split('\n')[1])
 
 # Execute the command to create a new state
-create_state_command = f"mysql -h {host} -u {user} -p{password} -D {database} -e 'INSERT INTO states (name) VALUES (\"California\");'"
+create_state_command = "mysql -h {} -u {} -p{} -D {} -e 'INSERT INTO states (name) VALUES (\"California\");'".format(host, user, password, database)
 os.system(create_state_command)
 
 # Get the updated count of records in the table
-updated_count_command = f"mysql -h {host} -u {user} -p{password} -D {database} -e 'SELECT COUNT(*) FROM states;'"
+updated_count_command = "mysql -h {} -u {} -p{} -D {} -e 'SELECT COUNT(*) FROM states;'".format(host, user, password, database)
 updated_count_output = os.popen(updated_count_command).read().strip()
 updated_count = int(updated_count_output.split('\n')[1])
 
