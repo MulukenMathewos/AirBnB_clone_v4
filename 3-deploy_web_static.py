d47b4da1f498bbd63ae2f426ba2b1fb1288724a0
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-08 16:50:13 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.146.67.252', '54.90.47.165']
+env.hosts = ['localhost']
 
 
 def do_pack():
@@ -32,15 +32,15 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}{}/'.format(path, no_ext))
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('sudo rm /tmp/{}'.format(file_n))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        run('sudo chown -R ubuntu:ubuntu {}{}'.format(path, no_ext))
-        run('sudo chmod -R 755 {}{}'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('chown -R ubuntu:ubuntu {}{}'.format(path, no_ext))
+        run('chmod -R 755 {}{}'.format(path, no_ext))
         print("New version deployed!")
         return True
     except ValueError:
