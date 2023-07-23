f682b0625eb35d70cd44f08c45811dce263ab928
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 18:44:40 +0300

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -3,6 +3,13 @@
 Contains the TestAmenityDocs classes
 """
 
+import os
+import sys
+
+# Add the parent directory to the Python path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
 from datetime import datetime
 import inspect
 import models
