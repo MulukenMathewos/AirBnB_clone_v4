f3ddcc2d928e0cd5b6cdcacc2bba5c42a2861894
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 19:08:32 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 # Add the parent directory to the Python path
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(parent_dir)
 
 from datetime import datetime
