a6cc3f6d22172c79a15871baa418e8977598d1e4
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 18:30:22 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,9 @@
 initialize the models package
 """
 
+import os
+import sys
+
 # Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
