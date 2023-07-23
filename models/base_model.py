ce21d4ae47786ff01642487983a9c19f5c9d3331
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 18:29:57 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,9 @@
 Contains class BaseModel
 """
 
+import os
+import sys
+
 # Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
