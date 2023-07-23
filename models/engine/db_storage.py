94659b3d31dca1c5871474bfbce8d26dc782203f
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 18:41:02 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,7 +7,8 @@ import os
 import sys
 
 # Add the parent directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
 
 import models
 from models.amenity import Amenity
