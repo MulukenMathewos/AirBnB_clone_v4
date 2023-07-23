f209d695738e844e699e2170f9546e59c736786a
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 19:47:57 +0300

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,19 +5,17 @@ Contains the TestUserDocs classes
 
 import os
 import sys
-
-# Add the parent directory to the Python path
-parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(parent_dir)
-
 from datetime import datetime
 import inspect
-import models
 from models import user
 from models.base_model import BaseModel
 import pep8
 import unittest
 User = user.User
+
+# Add the parent directory to the Python path
+parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(parent_dir)
 
 
 class TestUserDocs(unittest.TestCase):
