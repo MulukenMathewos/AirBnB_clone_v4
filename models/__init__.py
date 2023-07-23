c22f95ca27f92a79aa64873898575d529c6c88ec
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 18:25:06 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,9 @@
 initialize the models package
 """
 
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from os import getenv
 
 
