d2460570317271ae4131faca596a23a8103fe79f
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-23 18:46:29 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,6 +3,13 @@
 Contains the class TestConsoleDocs
 """
 
+import os
+import sys
+
+# Add the parent directory to the Python path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
 import console
 # import inspect
 import pep8
